459de2ae730d7cccd4a65dcc4fa5663c4ad928e4
bcjaeger <byron.jaeger@gmail.com>
2024-08-17 09:01:03 -0400

    probs for th's question

--- app.R
+++ app.R
@@ -10,9 +10,10 @@ ui <- fluidPage(
     sidebarPanel(
       numericInput("alpha_prior", "Alpha Prior (α):", value = 1, min = 0.1, step = 0.1),
       numericInput("beta_prior", "Beta Prior (β):", value = 10, min = 0.1, step = 0.1),
-      numericInput("n_mci", "Number of MCI cases observed (y):", value = 3, min = 0, step = 1),
+      numericInput("n_mci", "Number of MCI cases observed (y):", value = 1, min = 0, step = 1),
       numericInput("n_sampled", "Sample Size (n):", value = 50, min = 1, step = 1),
-      numericInput("min_prob_thresh", "Minimum Probability Threshold:", value = 0.05, min = 0.01, step = 0.01)
+      numericInput("min_prob_thresh", "Minimum Probability Threshold:", value = 0.05, min = 0.01, step = 0.01),
+      numericInput("min_mci_remain", "Minimum MCI count:", value = 5, min = 1, step = 1),
     ),
 
     mainPanel(
@@ -51,8 +52,16 @@ server <- function(input, output) {
       mutate(y = rbinom(100000, size = input$n_sampled, prob = pi)) %>%
       filter(y == input$n_mci)
 
-    paste("Posterior Probability that π <", input$min_prob_thresh, ":",
-          mean(sim$pi < input$min_prob_thresh))
+    # big text output: probability of
+    prob_final <- pbinom(q = input$min_mci_remain,
+                         size = 250 - input$n_sampled,
+                         prob = mean(sim$pi))
+
+    paste0("Posterior Probability that π <", input$min_prob_thresh, ": ",
+          mean(sim$pi < input$min_prob_thresh), "\n",
+          "Probability of at most ", input$min_mci_remain,
+          " MCI cases in the remaining ", 250 - input$n_sampled,
+          " participants: ", round(prob_final, 2))
   })
 
 }
